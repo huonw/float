f5427e4fcd6fd36b49e705d3d3ee3a143a569890
Huon Wilson <dbau.pp+github@gmail.com>
2015-11-01 00:38:35 +1100

    Add benchmarks for sqrt.
        test p00024::noop ... bench:          90 ns/iter (+/- 8)
        test p00024::sqrt ... bench:       1,109 ns/iter (+/- 183)
        test p00053::noop ... bench:          88 ns/iter (+/- 7)
        test p00053::sqrt ... bench:       2,262 ns/iter (+/- 856)
        test p00100::noop ... bench:         113 ns/iter (+/- 14)
        test p00100::sqrt ... bench:       5,470 ns/iter (+/- 1,846)
        test p01000::noop ... bench:       1,003 ns/iter (+/- 1,257)
        test p01000::sqrt ... bench:      81,509 ns/iter (+/- 20,265)
        test p10000::noop ... bench:       1,279 ns/iter (+/- 94)
        test p10000::sqrt ... bench:   4,305,098 ns/iter (+/- 3,736,861)
    
    cc #9

---
+++ benches/roots_powers.rs
@@ -0,0 +1,63 @@
+#![feature(test, augmented_assignments)]
+
+extern crate float;
+
+extern crate rand;
+extern crate test;
+
+use float::Float;
+use rand::{Rng, XorShiftRng};
+
+fn random(rng: &mut XorShiftRng, p: u32) -> Float {
+    let mut f = Float::rand(rng, p);
+    let exp = rng.gen::<i32>();
+    if rng.gen() {
+        f = -f;
+    }
+
+    f.mul_exp2(exp as i64)
+}
+
+fn bench<F>(b: &mut test::Bencher, p: u32, mut f: F)
+    where F: FnMut(Float) -> Float
+{
+    let mut rng = rand::random::<XorShiftRng>();
+    b.iter(|| {
+        // sqrt is somewhat dependent on the exact float it's working
+        // on, so we need to test a lot of them so the differences
+        // average out and hence the generation needs to be done
+        // regularly
+        f(random(&mut rng, p))
+    })
+}
+
+macro_rules! benches {
+    (with_prec $p: expr, $n: ident =>
+     $($name: ident, $e: expr;)*) => {
+        $(#[bench]
+          fn $name(b: &mut ::test::Bencher) {
+              ::bench(b, $p, |$n| $e);
+          })*
+    };
+
+    ($($name: ident, $p: expr;)*) => {
+        $(
+            mod $name {
+                #[bench]
+                fn noop(b: &mut ::test::Bencher) {
+                    ::bench(b, $p, |x| x)
+                }
+
+                benches!(with_prec $p, x =>
+                         sqrt, x.sqrt(););
+            })*
+    }
+}
+
+benches! {
+    p00024, 24;
+    p00053, 53;
+    p00100, 100;
+    p01000, 1000;
+    p10000, 10000;
+}
